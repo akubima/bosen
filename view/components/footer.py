48a1ae33cda925e42563705722f71ae68935c539
Bima <63215668+akubima@users.noreply.github.com>
2025-06-02 10:25:39 +0700

    updating footer.py

--- view/components/footer.py
+++ view/components/footer.py
@@ -7,7 +7,9 @@ class ViewComponentFooter(ViewsComponentsABC):
         super().__init__(frame)
 
     def render(self):
-        ctk.CTkLabel(master=self.frame, text=os.getenv('APP_DESCRIPTION') + ' \u00B7 \u00A9 2025 Bima Mukhlisin Bil Sajjad, Revaldi Rifwianda, Dafa Dzaki Putra Husada under MIT License \u00B7 Version ' + os.getenv('APP_VERSION'), font=ctk.CTkFont(size=12)).grid(padx=10)
+        ctk.CTkLabel(master=self.frame, text=os.getenv('APP_DESCRIPTION') + ' \u00B7 \u00A9 2025 Bima M. B. Sajjad, '
+                                                                            'Revaldi R., Dafa D. P. Husada '
+                                                                            'under MIT License \u00B7 Version ' + os.getenv('APP_VERSION'), font=ctk.CTkFont(size=12)).grid(padx=10)
 
     def hide(self):
         for child in self.frame.winfo_children():
